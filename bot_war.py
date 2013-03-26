b03584f3e13d1242d160f7e20087b5fbdc6e1f14
Alexander Z. <lulzsecer@gmail.com>
2013-03-26 19:21:25 +0200

    Update bot_war.py

--- bot_war.py
+++ bot_war.py
@@ -14,12 +14,12 @@ class GameBot():
 			"User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.40 Safari/537.31",
 			"Accept-Encoding": "deflate",
 			"sign-code":"",
-			"signin-authKey": "", #auth-key
-			"signin-userId": "", #your vkID [use Wireshark for this Luke]
+			"signin-authKey": "", # wireshark Luke
+			"signin-userId":  "", # wireshark
 			"server-method": self.method,
 			"client-ver": 403
 		}
-		self.index = 0		
+		self.index = 0
 		self.params = '{"x":0,"s":0,"n":0,"i":' + str(self.index) + ',"o":0,"c":6,"f":5,"d":null,"g":null}'
 		self.url = 'http://173.244.186.146/GeoVk/Segment01/segment.ashx'
 		self.counter = 1
@@ -30,24 +30,30 @@ class GameBot():
 		return self.header
 
 	'''====== send request to game server ======='''
-	def connect(self):
+	def connect(self, buyed=False):
 		# 'Skill maximum level reached'
-		req = urllib2.Request(self.url, self.params, self.set_sing_code())
-		response = urllib2.urlopen(req)	
-		if self.buyed == False:	
-			return zlib.decompress(response.read(), 16 + zlib.MAX_WBITS)
-		else:
-			return 'Good'
+		try:
+			req = urllib2.Request(self.url, self.params, self.set_sing_code())
+			response = urllib2.urlopen(req)
+			if buyed == False:
+				return zlib.decompress(response.read(), 16 + zlib.MAX_WBITS)
+			else:
+				try:
+					return json.loads(response.read()[1:])["m"] # msg output
+				except:
+					return 'Buyed'
+		except:
+			None
 	'''====== check progress of skill improvement ======='''
 	# Todo: check number of skill (increment bruteforce?)
 	def progress(self):
-		#print self.index, self.header["server-method"]		
-		self.index += 6				
-		self.buyed = False		
+		#print self.index, self.header["server-method"]
+		self.index += 6
+		self.buyed = False
 		data = json.loads(self.connect()[3:-33]) # get offers
 		for items in data.get('o'):			#accept offers for money
 			#print items
-			if (int(items["s"]["r"]["m"]) >= 1000):
+			if (int(items["s"]["r"]["m"]) >= 3000):
 				print "BuyThis -> Money: %s\tTitanium: %s\tUranium: %s" % (items["o"]["r"]["m"], items["o"]["r"]["t"], items["o"]["r"]["u"]) # buy
 				print "SoldThis -> Money: %s\tTitanium: %s\tUranium: %s" % (items["s"]["r"]["m"], items["s"]["r"]["t"], items["s"]["r"]["u"]) # sold
 				self.header['server-method'] = "TradingOffers.AcceptOffer"
@@ -55,16 +61,14 @@ class GameBot():
 				self.params = str(pp).replace("'", '"').replace(" ", "")
 				self.buyed = True
 				self.set_sing_code()
-				self.connect()
+				print self.connect(self.buyed)
 				if(self.index // 6 > 5 or self.buyed == True):
-					self.index = 0		
-					self.params = '{"x":0,"s":0,"n":0,"i":' + str(self.index) + ',"o":0,"c":6,"f":5,"d":null,"g":null}'	
+					self.index = 0
+					self.params = '{"x":0,"s":0,"n":0,"i":' + str(self.index) + ',"o":0,"c":6,"f":5,"d":null,"g":null}'
 					self.header['server-method'] = "TradingOffers.GetOffers"
-					self.set_sing_code()				
-				
-				print "===" * 20
-		
+					self.set_sing_code()
 
+				print "===" * 20
 
 
 bot = GameBot()
@@ -72,3 +76,4 @@ while True:
 	bot.progress()
 	if bot.index == 0:
 		time.sleep(7)
+
